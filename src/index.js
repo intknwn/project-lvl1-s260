25da22c540d94cd4a1d87fa2416d4955f63354cd
intknwn <intknwn@gmail.com>
2018-04-17 18:24:39 +0800

    Step 3 commit & step 2 fixes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import readlineSync from 'readline-sync';
 
-console.log('Welcome to the Brain Games!\n');
-const userName = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${userName}!`);
+export default () => {
+  console.log('Welcome to the Brain Games!\n');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+};
